3512b65a57df16a4831ed51d67714d66a4a25e17
Caleb Clausen & Dave Willett <dwillett@truecar.cam>
2012-08-31 15:07:01 -0700

    updated error msg/install instructions to help people get keyczar_cpp installed

--- ext/keyczar_ruby/extconf.rb
+++ ext/keyczar_ruby/extconf.rb
@@ -5,6 +5,13 @@ extension_name = 'keyczar_ruby'
 dir_config(extension_name)
 dir_config("keyczar", "/usr/local/include", "/usr/local/lib")
 
-have_library("keyczar") or raise
+have_library("keyczar") or raise RuntimeError, <<-MSG
+Could not find keyczar C++ library. Are you sure you have it installed?
+**
+** Source files can be found here:
+** http://code.google.com/p/keyczar/downloads/list
+**
+** see README.rdoc for more details
+MSG
 
 create_makefile(extension_name)
