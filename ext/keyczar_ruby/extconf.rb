1ee05976c0a28317facd15b7579ac7e2f132ba9d
Caleb Clausen & Dave Willett <dwillett@truecar.cam>
2012-09-07 14:30:40 -0700

    Allow specifying the KEYCZAR_PATH_PREFIX to modify location to search for lib and include

--- ext/keyczar_ruby/extconf.rb
+++ ext/keyczar_ruby/extconf.rb
@@ -1,9 +1,11 @@
 require 'mkmf'
 
+keyczar_path_prefix = ENV["KEYCZAR_PATH_PREFIX"] || "/usr/local"
+
 extension_name = 'keyczar_ruby'
 
 dir_config(extension_name)
-dir_config("keyczar", "/usr/local/include", "/usr/local/lib")
+dir_config("keyczar", File.join(keyczar_path_prefix, 'include'), File.join(keyczar_path_prefix, 'lib'))
 
 have_library("keyczar") or raise RuntimeError, <<-MSG
 Could not find keyczar C++ library. Are you sure you have it installed?
